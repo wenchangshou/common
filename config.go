054581a5bfbcf29731caa80402d61fad457ac87a
root <wcs@localhost.localdomain>
2022-11-14 13:11:39 +0800

    init

---
+++ config.go
@@ -0,0 +1,23 @@
+package common
+
+import (
+	"strconv"
+
+	"github.com/asim/go-micro/plugins/config/source/consul/v3"
+	"github.com/asim/go-micro/v3/config"
+)
+
+func GetConsulConfig(host string, port int64, prefix string) (config.Config, error) {
+	consulSource := consul.NewSource(
+		consul.WithAddress(
+			host+":"+strconv.FormatInt(port, 10)),
+		consul.WithPrefix(prefix),
+		consul.StripPrefix(true),
+	)
+	conf, err := config.NewConfig()
+	if err != nil {
+		return conf, err
+	}
+	err = conf.Load(consulSource)
+	return conf, err
+}
